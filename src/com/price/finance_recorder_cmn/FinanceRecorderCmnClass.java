b5ed0e99ed355ecef34e7c4e38b6aa2d3b704646
Price Tseng <everett6802@hotmail.com>
2016-07-21 13:43:37 +0800

    Implement the CompanyGroupSet class

--- src/com/price/finance_recorder_cmn/FinanceRecorderCmnClass.java
+++ src/com/price/finance_recorder_cmn/FinanceRecorderCmnClass.java
@@ -1,8 +1,11 @@
 package com.price.finance_recorder_cmn;
 
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.price.finance_recorder_cmn.FinanceRecorderCmnClassCompanyProfile.TraverseEntry;
 
 
 public class FinanceRecorderCmnClass 
@@ -447,69 +450,74 @@ public class FinanceRecorderCmnClass
 		return add_query(query_set, source_type, field_index);
 	}
 
-	public static class CompanyGroupSet
+	public static class CompanyGroupSet implements Iterable<Map.Entry<Integer, ArrayList<String>>>
 	{
 		private static TreeMap<Integer, ArrayList<String>> whole_company_number_in_group_map;
 		private TreeMap<Integer, ArrayList<String>> company_number_in_group_map = null;
+		private TreeMap<Integer, ArrayList<String>> altered_company_number_in_group_map = null;
+		private boolean is_add_done = false;
 
 		static void init_whole_company_number_in_group_map()
 		{
-			if (whole_company_number_in_group_map == null)
+			assert whole_company_number_in_group_map != null : "whole_company_number_in_group_map is NOT null";
+
+			FinanceRecorderCmnClassCompanyProfile company_profile = FinanceRecorderCmnClassCompanyProfile.get_instance();
+			whole_company_number_in_group_map = new TreeMap<Integer, ArrayList<String>>();
+			int company_group_size = company_profile.get_company_group_size();
+			for (int i = 0 ; i < company_group_size ; i++)
 			{
-				FinanceRecorderCmnClassCompanyProfile company_profile = FinanceRecorderCmnClassCompanyProfile.get_instance();
-				whole_company_number_in_group_map = new TreeMap<Integer, ArrayList<String>>();
-				int company_group_size = company_profile.get_company_group_size();
-				for (int i = 0 ; i < company_group_size ; i++)
-				{
-					FinanceRecorderCmnClassCompanyProfile.TraverseEntry traverse_entry = company_profile.group_entry(i);
-					ArrayList<String> company_number_array = new ArrayList<String>();
-					for (ArrayList<String> entry : traverse_entry)
-					{
-						company_number_array.add(entry.get(FinanceRecorderCmnClassCompanyProfile.COMPANY_PROFILE_ENTRY_FIELD_INDEX_COMPANY_CODE_NUMBER));
-					}
-					whole_company_number_in_group_map.put(i, company_number_array);
-				}
+				FinanceRecorderCmnClassCompanyProfile.TraverseEntry traverse_entry = company_profile.group_entry(i);
+				ArrayList<String> company_number_array = new ArrayList<String>();
+				for (ArrayList<String> entry : traverse_entry)
+					company_number_array.add(entry.get(FinanceRecorderCmnClassCompanyProfile.COMPANY_PROFILE_ENTRY_FIELD_INDEX_COMPANY_CODE_NUMBER));
+				whole_company_number_in_group_map.put(i, company_number_array);
 			}
 		}
 
-		public static class TraverseEntry implements Iterable<String>
+		static public CompanyGroupSet get_whole_company_group_set()
 		{
-			private ArrayList<String> company_group_array = null;
+			CompanyGroupSet company_group_set = new CompanyGroupSet();
+			company_group_set.add_done();
+			return company_group_set;
+		}
 
-			public TraverseEntry(ArrayList<String> new_array)
+		public CompanyGroupSet(){}
+
+		@Override
+		public Iterator<Map.Entry<Integer, ArrayList<String>>> iterator()
+		{
+			if (!is_add_done)
 			{
-				company_group_array = new_array;
+				String errmsg = "The add_done flag is NOT set to True";
+				FinanceRecorderCmnDef.format_error(errmsg);
+				throw new IllegalStateException(errmsg);
 			}
-			@Override
-			public Iterator<String> iterator()
+			Iterator<Map.Entry<Integer, ArrayList<String>>> it = new Iterator<Map.Entry<Integer, ArrayList<String>>>()
 			{
-				Iterator<String> it = new Iterator<String>()
+				private Iterator<Map.Entry<Integer, ArrayList<String>>> iter = altered_company_number_in_group_map.entrySet().iterator();
+				@Override
+				public boolean hasNext()
 				{
-					private int array_size = company_group_array.size();
-					private int cur_index = 0;
-					@Override
-					public boolean hasNext()
-					{
-						return cur_index < array_size;
-					}
-					@Override
-					public String next()
-					{
-						return company_group_array.get(cur_index++);
-					}
-					@Override
-					public void remove() {throw new UnsupportedOperationException();}
-				};
-				return it;
-			}
-		};
-
-		public CompanyGroupSet()
-		{
+					return iter.hasNext();
+				}
+				@Override
+				public Map.Entry<Integer, ArrayList<String>> next()
+				{
+					return (Map.Entry<Integer, ArrayList<String>>)iter.next();
+				}
+				@Override
+				public void remove() {throw new UnsupportedOperationException();}
+			};
+			return it;
 		}
 
 		public short add_company_list(int company_group_number, ArrayList<String> company_code_number_in_group_array)
 		{
+			if (is_add_done)
+			{
+				FinanceRecorderCmnDef.format_error("The add_done flag has already been set to True");
+				return FinanceRecorderCmnDef.RET_FAILURE_INCORRECT_OPERATION;
+			}
 			if (company_number_in_group_map == null)
 				company_number_in_group_map = new TreeMap<Integer, ArrayList<String>>();
 			if (!company_number_in_group_map.containsKey(company_group_number))
@@ -546,6 +554,12 @@ public class FinanceRecorderCmnClass
 
 		public short add_company_group(int company_group_number)
 		{
+			if (is_add_done)
+			{
+				FinanceRecorderCmnDef.format_error("The add_done flag has already been set to True");
+				return FinanceRecorderCmnDef.RET_FAILURE_INCORRECT_OPERATION;
+			}
+
 			if (company_number_in_group_map == null)
 				company_number_in_group_map = new TreeMap<Integer, ArrayList<String>>();
 
@@ -556,6 +570,49 @@ public class FinanceRecorderCmnClass
 			}
 			company_number_in_group_map.put(company_group_number, null);
 			return FinanceRecorderCmnDef.RET_SUCCESS;
+		}
+
+		public short add_done()
+		{
+			if (is_add_done)
+			{
+				FinanceRecorderCmnDef.format_error("The add_done flag has already been set to True");
+				return FinanceRecorderCmnDef.RET_FAILURE_INCORRECT_OPERATION;
+			}
+			setup_for_traverse();
+			is_add_done = true;
+			return FinanceRecorderCmnDef.RET_SUCCESS;
+		}
+
+		final ArrayList<String> get_company_number_in_group_list(int company_group_index)
+		{
+			if (!is_add_done)
+			{
+				String errmsg = "The add_done flag is NOT set to True";
+				FinanceRecorderCmnDef.format_error(errmsg);
+				throw new IllegalStateException(errmsg);
+			}
+			if (!altered_company_number_in_group_map.containsKey(company_group_index))
+			{
+				String errmsg = String.format("The company group index[%d] is NOT found in data structure", company_group_index);
+				FinanceRecorderCmnDef.format_error(errmsg);
+				throw new IllegalArgumentException(errmsg);
+			}
+			return altered_company_number_in_group_map.get(company_group_index);
+		}
+
+		private void setup_for_traverse()
+		{
+			if (whole_company_number_in_group_map == null)
+				init_whole_company_number_in_group_map();
+			if (company_number_in_group_map == null)
+				altered_company_number_in_group_map = whole_company_number_in_group_map;
+			else
+			{
+				altered_company_number_in_group_map = new TreeMap<Integer, ArrayList<String>>();
+				for (Map.Entry<Integer, ArrayList<String>> entry : company_number_in_group_map.entrySet())
+					altered_company_number_in_group_map.put(entry.getKey(), (entry.getValue() != null) ? entry.getValue() : whole_company_number_in_group_map.get(entry.getKey()));
+			}
 		}
 	};
 
